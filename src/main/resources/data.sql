6279221fe11566918777bc30a1ed85840b10327d
changuii <rhljh7410@gmail.com>
2025-06-10 15:36:08 +0900

    chore: 예약 시간 임시 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,14 @@ INSERT INTO restaurant(name) VALUES
    ('차이나스토리'),
    ('행복한초밥'),
    ('버섯가게');
+
+INSERT INTO reservation_time(time, restaurant_id) VALUES
+  ('12:00', 1),
+  ('12:00', 2),
+  ('12:00', 3),
+  ('12:00', 4),
+  ('13:00', 2),
+  ('14:00', 2),
+  ('15:00', 2),
+  ('16:00', 2),
+  ('17:00', 2);
